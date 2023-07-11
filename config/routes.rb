dd44ab20ab100539f2d5ea6682550975a7c333dc
yuritakagi <takagiyuri0712@gmail.com>
2023-07-11 16:48:39 +0900

    change: ルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  get 'user/new'
-  get 'user/create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resource :user, only: %i[new create]
 end
