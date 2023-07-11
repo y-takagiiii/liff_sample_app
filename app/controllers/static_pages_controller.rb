014a3e79c7d9ed5f095dc7229fee3a1832da4a6e
yuritakagi <takagiyuri0712@gmail.com>
2023-07-11 16:58:12 +0900

    add: top、after_loginアクションを作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,11 @@
 class StaticPagesController < ApplicationController
   def top
+    render current_user.nil? ? :before_login : :after_login
   end
 
-  def before_login
-  end
+  def before_login; end
 
   def after_login
+    login_required
   end
 end
