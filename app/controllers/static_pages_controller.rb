7265a972bd2fba6c6646befa2ceeb4b675fb6cc5
yuritakagi <takagiyuri0712@gmail.com>
2023-07-11 16:55:21 +0900

    add: bundle exec rails g controller StaticPages top before_login after_loginを実行

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def top
+  end
+
+  def before_login
+  end
+
+  def after_login
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/top'
+  get 'static_pages/before_login'
+  get 'static_pages/after_login'
   resource :user, only: %i[new create]
 end
