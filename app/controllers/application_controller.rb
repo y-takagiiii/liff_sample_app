86e0e468121e62833f79caa9def57ecf3885dd8a
yuritakagi <takagiyuri0712@gmail.com>
2023-07-11 16:54:27 +0900

    add: ログイン周りのヘルパーメソッドを定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :login_required, :logged_in?
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def login_required
+    redirect_to root_path unless current_user
+  end
+
+  def logged_in?
+    current_user.present?
+  end
 end
